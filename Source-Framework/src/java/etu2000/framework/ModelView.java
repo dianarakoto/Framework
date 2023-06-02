d65f1a6a7e567cca355f7c5e24fe32d6ec80b8a6
Diana Rakotomaharo <dianarakoto9@gmail.com>
2023-06-02 08:12:29 +0300

    ModelView

---
+++ Source-Framework/src/java/etu2000/framework/ModelView.java
@@ -0,0 +1,17 @@
+package etu2000.framework;
+
+public class ModelView {
+    String view;
+
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+    
+    public ModelView (String view){
+        this.view = view;
+    }
+}
---
+++ Source-Framework/src/java/etu2000/framework/annotation/Url.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu2000.framework.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Url {
+   String value() default "/";
+}
